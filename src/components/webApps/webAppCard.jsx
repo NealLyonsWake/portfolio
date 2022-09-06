d65148ea77fc39d0646552f16b76701ac07d4ae2
NealLyonsWake <waken.games@gmail.com>
2022-09-06 23:24:17 +0100

    New webAppPopUp component created to manage mobile responses for mobile screen sizes.

--- src/components/webApps/webAppCard.jsx
+++ src/components/webApps/webAppCard.jsx
@@ -1,4 +1,4 @@
-import WebAppTech from './webAppTech';
+import WebAppPopUp from './webAppPopUp';
 
 function WebAppCard(props) {
 
@@ -8,47 +8,21 @@ function WebAppCard(props) {
         return images;
     }
 
-    function showSiteLink() {
-        if (props.showSite) return <a href={props.siteURL} target='_blank'><button>Site</button></a>;
-        else return;
-    }
-
-    function showDesignLink() {
-        if (props.showDesign) return <a href={props.designURL} target='_blank'><button>Design</button></a>;
-        else return;
-    }
-
     const images = importAll(require.context('../../thumbnails/', false, /\.(png|jpe?g|svg)$/));
 
     return (
         <div className='project-card'>
             <img className='card-poster' src={images[props.fileName]} alt={props.imgAlt} />
-            <div id={props.title}>
-                <div className='card-overview'>
-                    <div className='project-description'>
-                        <h5>{props.title}:</h5>
-                        <p>{props.overview}</p>
-                    </div>
-                    <div className='technologies'>
-                        <h5>Main Technologies Used:</h5>
-                        <ul>
-                            {props.tech.map((t, index) => {
-                                return (
-                                    <WebAppTech
-                                        key={index}
-                                        tech={t}
-                                    />
-                                )
-                            })}
-                        </ul>
-                    </div>
-                    <div className='visit-buttons'>
-                        {showSiteLink()}
-                        {showDesignLink()}
-                        <a href={props.codeURL} target='_blank'><button>Code</button></a>
-                    </div>
-                </div>
-            </div>
+            <WebAppPopUp
+                title={props.title}
+                overview={props.overview}
+                tech={props.tech}
+                showSite={props.showSite}
+                siteURL={props.siteURL}
+                showDesign={props.showDesign}
+                designURL={props.designURL}
+                codeURL={props.codeURL}
+            />
         </div>
     )
 }
---
+++ src/components/webApps/webAppPopUp.jsx
@@ -0,0 +1,55 @@
+import React, {useEffect, useState} from 'react';
+import WebAppTech from './webAppTech';
+
+
+function WebAppPopUp(props) {
+    const [screenSize, setScreenSize] = useState();
+
+    function showSiteLink() {
+        if (props.showSite) return <a href={props.siteURL} target='_blank'><button>Site</button></a>;
+        else return;
+    }
+
+    function showDesignLink() {
+        if (props.showDesign) return <a href={props.designURL} target='_blank'><button>Design</button></a>;
+        else return;
+    }
+
+    window.addEventListener('resize', () =>{
+        setScreenSize(window.innerWidth);
+    })
+
+    function checkScreenSize(){
+        if (screenSize <= 620) return 'mobile-card-overview';
+        else return 'card-overview';
+    }
+
+    return (
+        <div className={checkScreenSize()}>
+            <div className='project-description'>
+                <h5>{props.title}:</h5>
+                <p>{props.overview}</p>
+            </div>
+            <div className='technologies'>
+                <h5>Main Technologies Used:</h5>
+                <ul>
+                    {props.tech.map((t, index) => {
+                        return (
+                            <WebAppTech
+                                key={index}
+                                tech={t}
+                            />
+                        )
+                    })}
+                </ul>
+            </div>
+            <div className='visit-buttons'>
+                {showSiteLink()}
+                {showDesignLink()}
+                <a href={props.codeURL} target='_blank'><button>Code</button></a>
+            </div>
+        </div>
+    )
+}
+
+export default WebAppPopUp;
